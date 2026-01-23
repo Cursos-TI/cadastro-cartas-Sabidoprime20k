08d7c00d99c56ed23bcd120820f7f5740e0837cc
Sabidoprime20k <andre10lg2016@gmail.com>
2026-01-23 09:57:41 +0000

    Configuração nova do super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -37,14 +37,20 @@ scanf("%s", iniciar);
   printf("Quantidade de pontos turiticos: \n");
   scanf("%d", &turista);
 
-printf("Segue abaixo a primeira carta cadastrada:");
-printf("Estado: %s \n", estado);
-printf("Codigo: %s \n", codigo_carta);
-printf("Cidade: %s \n", nome_cidade);
-printf("Habitantes: %lf \n", população);
-printf("Kms área: %f\n", área);
-printf("PIB: %f \n", pib);
-printf("Quantidade de PT: %d \n", turista);
+
+  float densidade = população / área;
+  float pib_per_capita = pib / população;
+
+  printf("Segue abaixo a primeira carta cadastrada:");
+  printf("Estado: %s \n", estado);
+  printf("Codigo: %s \n", codigo_carta);
+  printf("Cidade: %s \n", nome_cidade);
+  printf("Habitantes: %lf \n", população);
+  printf("Kms área: %f\n", área);
+  printf("PIB: %f \n", pib);
+  printf("Quantidade de PT: %d \n", turista);
+  printf("Densidade populacional: %f\n", densidade);
+  printf("PiB per capita: %f \n", pib_per_capita);
 
 
 char estado2[50], nome_cidade2[50], codigo_carta2[20];
@@ -52,7 +58,8 @@ int turista2;
 float área2, pib2;
 double população2;
 
-printf("Cadastro da segunda carta do trunfo. \n");
+
+  printf("Cadastro da segunda carta do trunfo. \n");
 
   printf("Escolha o estado: \n");
   scanf("%s", estado2);
@@ -75,14 +82,21 @@ printf("Cadastro da segunda carta do trunfo. \n");
   printf("Quantidade de pontos turisticos: \n");
   scanf("%d", &turista2);
 
+  float densidade2 = população2 / área2;
+  float pib_per_capita2 = pib2 / população2;
+
+
   printf("Segue abaixo a segunda carta cadastrada: \n");
   printf("Estado: %s \n", estado2);
   printf("Codigo: %s \n", codigo_carta2);
- printf("Cidade: %s \n", nome_cidade2);
- printf("Habitantes: %lf \n", população2);
- printf("KMs área: %f\n", área2);
- printf("PIB: %f \n", pib2);
- printf("Quantidade de PT: %d \n", turista2);
+  printf("Cidade: %s \n", nome_cidade2);
+  printf("Habitantes: %lf \n", população2);
+  printf("KMs área: %f\n", área2);
+  printf("PIB: %f \n", pib2);
+  printf("Quantidade de PT: %d \n", turista2);
+  printf("Densidade populacional: %f\n", densidade2);
+  printf("PiB per capita: %f \n", pib_per_capita2);
 
+  
 return 0;
 } 
